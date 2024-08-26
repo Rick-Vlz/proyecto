68d9601f7de2c303b5925d33ef711327ecf89e0e
Rick Valdes <ricardovlz.wlf@gmail.com>
2024-08-26 14:21:06 -0600

    v5.0.2

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
                 <section className="text-gray-600 body-font">
             <div className="container mx-auto flex px-5 pt-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Energía Solar Limpia y Sostenible?
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Energía Solar Limpia y Sostenible?&nbsp;
                         <br className="hidden lg:inline-block" />Solar Solutions
                     </h1>
                     <p className="mb-8 leading-relaxed">Solar Solutions es una empresa líder en energía solar. Nos dedicamos a proporcionar soluciones sostenibles y limpias para la generación de energía. Únete a nosotros en nuestro viaje hacia un mundo más brillante y sostenible</p>
