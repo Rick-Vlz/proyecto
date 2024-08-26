822491637953aa34cc41c57ddf5481a78191c1a6
Rick Valdes <ricardovlz.wlf@gmail.com>
2024-08-26 14:18:43 -0600

    v5.0.1

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -5,65 +5,80 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
-
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
     const userLoginReducer = useSelector((state) => state.userLoginReducer);
 
-    const{loading, error, userInfo} = userLoginReducer
-    const dispatch = useDispatch()
+    const { loading, error, userInfo } = userLoginReducer;
+    const dispatch = useDispatch();
+
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(userLoginAction(email, password))
-    }
+        dispatch(userLoginAction(email, password));
+    };
 
     return (
-        
-            <Layout>
-
-            {loading ? (<h1>loading</h1>) : error ? (<h1>{error}</h1>) : (
+        <Layout>
+            {loading ? (
+                <h1>loading</h1>
+            ) : error ? (
+                <h1>{error}</h1>
+            ) : (
                 <>
-                <form className="max-w-sm mx-auto h-[25rem] content-center" onSubmit={submitHandler}>
-                    <div className="mb-5">
-                        <label 
-                        htmlFor="email" 
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Email
-                        </label>
-                        <input 
-                        type="text" 
-                        id="text" 
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        placeholder="John Wick" 
-                        required 
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}/>
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                            Password
-                        </label>
-                        <input 
-                        type="password" 
-                        id="password" 
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        required 
-                        value={password}
-                        onChange={(e)=>setPassword(e.target.value)}/>
-                    </div>
-                    
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-                    <label htmlFor="text"> ó </label>
-                    <Link to="/register" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrarse</Link>
-                </form>
-                
-                
-                
-                
+                    <form className="max-w-sm mx-auto h-[25rem] content-center" onSubmit={submitHandler}>
+                        <div className="mb-5">
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Email
+                            </label>
+                            <input
+                                type="text"
+                                id="text"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="John Wick"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-5">
+                            <label
+                                htmlFor="password"
+                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                            >
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Login
+                        </button>
+                        
+                        {/* Centrar el botón de Registrarse */}
+                        <div className="mt-4 flex">
+                            <Link
+                                to="/register"
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                Registrarse
+                            </Link>
+                        </div>
+                    </form>
                 </>
             )}
-
-                
-            </Layout>
-    )
-};
+        </Layout>
+    );
+}
